bdaf05d95af2ce363e9314e9c53a6cc9a7c49ab4
javiro3004@gmail.com <javiro3004@gmail.com>
2024-10-08 19:05:49 -0500

    carrito

--- proyecto/carrito.js
+++ proyecto/carrito.js
@@ -1,7 +1,60 @@
-const btnCart = document.querySelector('.container-cart-icon');
-const containerCartProducts = document.querySelector(
-	'.container-cart-products'
-);
-btnCart.addEventListener('click', () => {
-	containerCartProducts.classList.toggle('hidden-cart');
+const agregarCarrito = document.querySelectorAll('.agregar-carrito');
+
+let carrito = [];
+
+function agregarProductoAlCarrito(producto, precio) {
+    const productoObj = {
+        producto: producto,
+        10000: precio
+    };
+
+    carrito.push(productoObj);
+
+    mostrarCarrito();
+}
+
+function eliminarProductoDelCarrito(indice) {
+    carrito.splice(indice, 1);
+
+    mostrarCarrito();
+}
+
+function mostrarCarrito() {
+    const contenedorCarrito = document.getElementById('carrito');
+
+    contenedorCarrito.innerHTML = '';
+
+    carrito.forEach((producto, indice) => {
+        const elemento = document.createElement('li');
+        elemento.classList.add('list-group-item');
+        elemento.textContent = `${producto.nombre} - $${producto.precio}`;
+
+        const botonEliminar = document.createElement('button');
+        botonEliminar.classList.add('btn', 'btn-danger', 'float-end');
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.onclick = () => {
+            eliminarProductoDelCarrito(indice);
+        };
+
+        elemento.appendChild(botonEliminar);
+        contenedorCarrito.appendChild(elemento);
+    });
+
+    const total = carrito.reduce((acumulado, producto) => acumulado + producto.precio, 0);
+    const elementoTotal = document.createElement('li');
+    elementoTotal.classList.add('list-group-item');
+    elementoTotal.textContent = `Total: $${total}`;
+    contenedorCarrito.appendChild(elementoTotal);
+
+    const contenedorCartProducts = document.querySelector('.container-cart-products');
+    contenedorCartProducts.classList.remove('hidden-cart');
+}
+
+agregarCarrito.forEach((boton) => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const producto = boton.getAttribute('data-producto');
+        const precio = parseInt(boton.getAttribute('data-precio'));
+        agregarProductoAlCarrito(producto, precio);
+    });
 });
